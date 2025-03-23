adc10b44da10bae54ae441d42a409ad98f925220
Sharp Hall <sharp@sharphall.org>
2025-03-23 12:26:16 -0400

    Move "nodes" into class instead of referencing by number

--- bottastic/bottastic.py
+++ bottastic/bottastic.py
@@ -4,7 +4,6 @@ import logging
 import sys
 from abc import ABC
 from concurrent.futures import ThreadPoolExecutor
-from typing import Optional
 
 import meshtastic
 import meshtastic.mesh_interface
@@ -29,6 +28,68 @@ def call_async(fn, *args, **kwargs):
 
 class NoEncryptionKey(Exception):
     pass
+
+
+class MeshtasticNode:
+    def __init__(self, bottastic: "Bottastic", num: int):
+        self.bottastic = bottastic
+        self.num = num
+
+    @property
+    def key(self):
+        if self.bottastic.interface.nodesByNum:
+            try:
+                return base64.b64decode(
+                    self.bottastic.interface.nodesByNum[self.num]["user"]["publicKey"]
+                )
+            except KeyError:
+                return None
+
+    @property
+    def short_name(self):
+        if self.bottastic.interface.nodesByNum:
+            try:
+                return self.bottastic.interface.nodesByNum[self.num]["user"][
+                    "shortName"
+                ]
+            except KeyError:
+                return None
+
+    @property
+    def long_name(self):
+        if self.bottastic.interface.nodesByNum:
+            try:
+                return self.bottastic.interface.nodesByNum[self.num]["user"]["longName"]
+            except KeyError:
+                return None
+
+    async def send_message(
+        self,
+        text: str,
+        require_encryption=True,
+        want_response=False,
+    ):
+        key = self.key
+        if not key and require_encryption:
+            raise NoEncryptionKey()
+
+        def _on_deliver(_):
+            if self.bottastic.echo_sent and self.bottastic.echo_recieved:
+                print(f"Delivered message to {self.num}: {text}")
+
+        await call_async(
+            self.bottastic.interface.sendData,
+            text.encode("utf-8"),
+            self.num,
+            wantAck=want_response,
+            wantResponse=want_response,
+            onResponse=_on_deliver if want_response else None,
+            pkiEncrypted=key is not None,
+            publicKey=key,
+            portNum=meshtastic.portnums_pb2.PortNum.TEXT_MESSAGE_APP,
+        )
+        if self.bottastic.echo_sent:
+            print(f"Sent to {self.num} ({'enc' if key else 'no enc'}): {text}")
 
 
 class Bottastic(ABC):
@@ -59,21 +120,22 @@ class Bottastic(ABC):
     def _handle_on_recieve(self, packet):
         if not self.loop or not self.my_node:
             return
-        user = None
         if packet["to"] != self.my_node["num"]:
             return
-        if self.interface.nodesByNum:
-            node = self.interface.nodesByNum.get(packet["from"])
-            if node:
-                user = node["user"]
         if "decoded" not in packet or "text" not in packet["decoded"]:
             return
         if self.echo_recieved:
             print(f"Message from {packet['from']}: {packet['decoded']['text']}")
         asyncio.run_coroutine_threadsafe(
-            self.handle_message(packet["from"], packet["decoded"]["text"], user),
+            self.handle_direct_message(
+                from_node=MeshtasticNode(self, packet["from"]),
+                message=packet["decoded"]["text"],
+            ),
             self.loop,
         )
+
+    def get_node_by_num(self, num: int):
+        return MeshtasticNode(self, num)
 
     def close(self):
         self.interface.close()
@@ -82,41 +144,7 @@ class Bottastic(ABC):
     async def on_initialized(self):
         pass
 
-    async def send_message(
-        self, to: int, text: str, require_encryption=True, want_response=False
-    ):
-        key = None
-        if (
-            self.interface.nodesByNum is not None
-            and "user" in self.interface.nodesByNum[to]
-            and "publicKey" in self.interface.nodesByNum[to]["user"]
-        ):
-            key = base64.b64decode(self.interface.nodesByNum[to]["user"]["publicKey"])
-        else:
-            if require_encryption:
-                raise NoEncryptionKey()
-
-        def _on_deliver(_):
-            if self.echo_sent and self.echo_recieved:
-                print(f"Delivered message to {to}: {text}")
-
-        await call_async(
-            self.interface.sendData,
-            text.encode("utf-8"),
-            to,
-            wantAck=want_response,
-            wantResponse=want_response,
-            onResponse=_on_deliver if want_response else None,
-            pkiEncrypted=key is not None,
-            publicKey=key,
-            portNum=meshtastic.portnums_pb2.PortNum.TEXT_MESSAGE_APP,
-        )
-        if self.echo_sent:
-            print(f"Sent to {to} ({'enc' if key else 'no enc'}): {text}")
-
-    async def handle_message(
-        self, from_id: int, message: str, from_user: Optional[dict]
-    ):
+    async def handle_direct_message(self, from_node: MeshtasticNode, message: str):
         pass
 
     async def event_loop(self):
@@ -159,11 +187,12 @@ pub.subscribe(
 
 
 class PingPongBot(Bottastic):
-    async def handle_message(
-        self, from_id: int, message: str, from_user: Optional[dict]
-    ):
+    async def handle_direct_message(self, from_node: MeshtasticNode, message: str):
         if message.strip().lower() == "ping":
-            await self.send_message(from_id, "pong!")
+            if from_node.long_name:
+                await from_node.send_message(f"pong! hello, {from_node.long_name}")
+            else:
+                await from_node.send_message("pong!")
 
 
 if __name__ == "__main__":
